dbb6d58db49d99fb195b083e2d079b30f1917065
Suresh133 <51027983+Suresh133@users.noreply.github.com>
2019-06-23 11:40:27 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,49 +1,104 @@
-from pyrogram import Client, Filters
+from pyrogram import Client, Filters,Emoji
+app = Client('my_account',728044,"a41ddadc9696482aff94a4b37221574a")
 
+u = '-1001187620145'
 
-app = Client('890398475:AAFKe-e5H2owXvzoB-AlIpy3ELE-0f7OoNs',605563,"7f2c2d12880400b88764b9b304e14e0b")
-
-
-
-u = '-1001277886997'
-s = '-1001378725482'
-
-
-@app.on_message(Filters.chat(int(s)) & Filters.text & ~ Filters.edited)
-
-def forawrd(client, message):
-
-    mes = client.send_message(int(u), "**" + message.text + "**")
-    file = open("sure.txt" , "r")
-    lines = file.readlines()
-    file.close()
-    for line in lines:
-         files = open("sure.txt" , "w")
-         files.write( line + " " + str(message.message_id) +  " " + str(mes.message_id))
-         files.close()       
-
-
-
+@app.on_message( Filters.text & ~Filters.edited)
+def forward(client, message):
+ fil = open("source.txt" , "r")
+ lins = file.readlines()
+ fil.close()
+ for t in lins:
+  if t == message.chat.id:
+    text = message.text
+    f = False
+    words = ['dekho','fix','😱','😢','😳','fixer','👆','👇','match','pass','sab','chase','defend','hai','karvana','link','loss','audio','varna','pura','puri','open','paid','contact','baazigar','market','load','whatsapp','timepass','kamma','book','teenpatti','diya','bhai','😀','😑','😐','😊','😜','😇','😎','😂','😘','😋','😝','🥺','members','🖕','member','only','chut','lund','gand','ma','maa','bhosdi','bahan','loude','lode','lavde','chutiya','🤞','🤟','☝️','mkc','bkc','mc','bc','madarchod','bahanchod','bahnchod','gandu','❓','kya','wbt','line','who',"https://",'joinchat','bullet','fuck','🤔','LUND',"?","LU"]
+    for word in words:
+        if word.casefold() in text.casefold():
+            f = True
+    if not f:
+        if '🖲' in message.text:
+            mes = client.send_message(int(u),"**" + message.text.replace('🖲' , '💘') + "**")
+            file = open("sure.txt" , "r")
+            lines = file.readlines()
+            file.close()
+            for line in lines:
+               files = open("sure.txt" , "w")
+               files.write( line + " " + str(message.message_id) +  " " + str(mes.message_id))
+               files.close()
+        elif '📟' in message.text :
+            mes = client.send_message(int(u),"**" + message.text.replace('📟' , '🏝') + "**")
+            file = open("sure.txt" , "r")
+            lines = file.readlines()
+            file.close()
+            for line in lines:
+               files = open("sure.txt" , "w")
+               files.write( line + " " + str(message.message_id) +  " " + str(mes.message_id))
+               files.close()
+        elif message.text == 'WD' :
+            client.send_message(int(u),'🤦‍♂️ **WIDE BALL** 🤦‍♂️')
+        elif message.text.casefold() == 'WKT'.casefold() :
+            client.send_message(int(u),'🚾** Wicket Wicket Wicket** 🚾 ') 
+        elif 'NO BALL' in message.text:
+            client.send_message(int(u),'🔛** NO BALL **🔛' )
+        elif 'DRINKS BREAK' in message.text:
+            client.send_message(int(u), '🍻** DRINKS BREAK **🍻') 
+        elif 'DEAD BALL' in message.text:
+            client.send_message(int(u), '🔁** DEAD BALL **🔄') 
+        elif message.text.casefold() == 'RUKA'.casefold():
+            client.send_message(int(u), '🛑** BOWLER RUKA **🛑')
+        elif message.text.casefold() == '🚾WICKET WICKET🚾'.casefold():
+            client.send_message(int(u),'🚾** Wicket Wicket Wicket **🚾')
+        else:
+            mes = client.send_message(int(u), "**" + message.text.replace('🎾' , '🥎') + "**")
+            file = open("sure.txt" , "r")
+            lines = file.readlines()
+            file.close()
+            for line in lines:
+               files = open("sure.txt" , "w")
+               files.write( line + " " + str(message.message_id) +  " " + str(mes.message_id))
+               files.close()
+         
 
 @app.on_message(Filters.chat(int(s)) & Filters.sticker)
-
 def forawrd(client, message):
-
-   client.send_sticker(int(u),message.sticker.file_id)
-            
-@app.on_message(Filters.chat(int(s)) & Filters.text & Filters.edited)
-
-def forawrd(client, message):
-
-  file = open("sure.txt" , "r")
-  lines = file.readlines()
-  file.close()
-  for line in lines:
+  if message.sticker.file_id == 'CAADBQADkgIAAlTquhpPMfzjWNqQagI' :
+    client.send_message(int(u),'🍾 **INNINIGS BREAK** 🍾' )
+    
+@app.on_message( Filters.text & Filters.edited)
+def forward(client, message):
+ fil = open("source.txt" , "r")
+ lins = file.readlines()
+ fil.close()
+ for t in lins:
+  if t == message.chat.id:
+   file = open("sure.txt" , "r")
+   lines = file.readlines()
+   file.close()
+   for line in lines:
     x = line.split()
     id = str(message.message_id)
     if id in x:
-      client.edit_message_text(int(u),int(x[x.index(id)+1]), "**" + message.text + "**" )
-
-
-
+     if '🖲' in message.text:
+        client.edit_message_text(int(u),int(x[x.index(id)+1]), "**" + message.text.replace('🖲' , '💘') + "**" )
+     elif '📟' in message.text :
+        client.edit_message_text(int(u),int(x[x.index(id)+1]),"**" + message.text.replace('📟' , '🏝') + "**")
+     else:
+        client.edit_message_text(int(u),int(x[x.index(id)+1]),"**" + message.text.replace('🎾' , '🥎')+ "**")
+ 
+   
+@app.on_message(Filters.command("clear"))
+def forward(client, message):
+  file = open("sure.txt" , "r")
+  file.write("001 002")
+  file.close()
+ 
+         
+   
+@app.on_message(Filters.command("set"))
+def forward(client, message):
+  with open("source.txt" , "r") as file:
+   file.write(message.text.split(' ')[1])
+   file.close()
+        
 app.run()
